11b405dcd699833525f0a8f5f10dfa9a0844925c
CrashGtaFan <gtafan51@gmail.com>
2019-03-06 07:38:57 +0000

    Add stock managment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def stock_error
+    reason =
+      if !current_user.under_stock_limit?
+        '10 stocks'
+      elsif current_user.stock_arleady_added?(@stock.ticker)
+        'that stock'
+      end
+    "Stock cannot be added because you added #{reason}"
+  end
 end
